09b0e002b7d59e2c89ddeb0850449a9bb47534ec
wuhuaqiang <609676374@qq.com>
2018-04-26 10:08:55 +0800

    电力系统前端1.0

--- src/views/systemmanagement/UnitMsg.vue
+++ src/views/systemmanagement/UnitMsg.vue
@@ -125,7 +125,7 @@ export default {
       listLoading: true,
       listQuery: {
         page: 1,
-        limit: 20,
+        limit: 10,
         importance: undefined,
         title: undefined,
         type: undefined,
